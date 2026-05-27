14e998e03550c59b1b47e3adf923d3450e57c543
alex <aledrive16@hotmail.com>
2026-05-26 18:15:38 -0600

    correcxiones

--- server.py
+++ server.py
@@ -675,5 +675,12 @@ def get_carnet_pdf(carnet):
     return jsonify({'message': 'Carnet PDF no disponible.'}), 404
 
 if __name__ == '__main__':
-    # Registrar servidor
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # Puerto dinámico para Render
+    port = int(os.environ.get("PORT", 5000))
+
+    # Ejecutar servidor Flask
+    app.run(
+        host='0.0.0.0',
+        port=port,
+        debug=False
+    )
